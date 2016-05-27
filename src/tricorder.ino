722bb3d5f046614d1538cff5a733de8183cadbe4
Sean Caulfield <sean@yak.net>
2016-05-27 15:10:06 -0400

    dump sensor data to serial

--- src/tricorder.ino
+++ src/tricorder.ino
@@ -26,11 +26,18 @@
 #include <Adafruit_VEML6070.h>
 
 #define BATT_DIV_PIN A7
-#define DEFAULT_PRESSURE
+
+#define BLE_CS  8
+#define BLE_IRQ 7
+#define BLE_RST 4
+
+#define DEFAULT_PRESSURE 1000.0
 
 ////////////////////////////////////////////////////////////////////////
-// GLOBAL SENSOR OBJECTS
+// GLOBAL OBJECTS
 ////////////////////////////////////////////////////////////////////////
+
+Adafruit_BluefruitLE_SPI ble(BLE_CS, BLE_IRQ, BLE_RST);
 
 Adafruit_BMP280 bmp280;
 Adafruit_BNO055 bno055;
@@ -72,15 +79,19 @@ void setup() {
   tsl2591.begin();
   veml6070.begin(VEML6070_1_T);
 
+  // Init serial for debugging BLE modem
+  while (!Serial)
+    delay(1);
+  Serial.begin(115200);
+  Serial.println("HAI");
+
   // Initialize BLE modem
+  ble.begin(true);
+  ble.echo(false);
+  ble.info();
 
   // For zee debugging
   pinMode(13, OUTPUT);
-
-  while (!Serial)
-    delay(1);
-  Serial.begin(9600);
-  Serial.println("HAI");
 
 }
 
@@ -106,6 +117,11 @@ void loop() {
   float press = bmp280.readPressure();
   float alt = bmp280.readAltitude(DEFAULT_PRESSURE);
 
+  Serial.println("BMP280");
+  Serial.print("T   = "); Serial.println(bmp280_temp);
+  Serial.print("P   = "); Serial.print(press); Serial.println("Pa");
+  Serial.print("Alt = "); Serial.print(alt); Serial.println("m");
+
   imu::Vector<3> euler = bno055.getVector(Adafruit_BNO055::VECTOR_EULER);
   float x = euler.x();
   float y = euler.y();
@@ -113,22 +129,56 @@ void loop() {
   uint8_t cal_sys, cal_gyro, cal_acc, cal_mag;
   bno055.getCalibration(&cal_sys, &cal_gyro, &cal_acc, &cal_mag);
 
+  Serial.println("BNO055");
+  Serial.print("X = "); Serial.println(x);
+  Serial.print("Y = "); Serial.println(y);
+  Serial.print("Z = "); Serial.println(z);
+  Serial.print("Calibration (Sys)  = "); Serial.println(cal_sys);
+  Serial.print("Calibration (Gyro) = "); Serial.println(cal_gyro);
+  Serial.print("Calibration (Acc)  = "); Serial.println(cal_acc);
+  Serial.print("Calibration (Mag)  = "); Serial.println(cal_mag);
+
   float ir_amb_c = mlx90614.readAmbientTempC();
   float ir_amb_f = mlx90614.readAmbientTempF();
   float ir_obj_c = mlx90614.readObjectTempC();
   float ir_obj_f = mlx90614.readObjectTempF();
 
+  Serial.println("MLX90614");
+  Serial.print("Ambient = "); Serial.print(ir_amb_c); Serial.println("C");
+  Serial.print("          "); Serial.print(ir_amb_f); Serial.println("F");
+  Serial.print("Object  = "); Serial.print(ir_obj_c); Serial.println("C");
+  Serial.print("          "); Serial.print(ir_obj_f); Serial.println("F");
+
   float sht31_temp = sht31.readTemperature();
   float humidity = sht31.readHumidity();
+
+  Serial.println("SHT31");
+  Serial.print("T = "); Serial.println(sht31_temp);
+  Serial.print("RH% = "); Serial.println(humidity);
 
   uint16_t r, g, b, c;
   tcs34725.getRawData(&r, &g, &b, &c);
   uint16_t colorTemp = tcs34725.calculateColorTemperature(r, g, b);
   uint16_t lux = tcs34725.calculateLux(r, g, b);
 
+  Serial.println("TCS34725");
+  Serial.print("R = "); Serial.println(r);
+  Serial.print("G = "); Serial.println(g);
+  Serial.print("B = "); Serial.println(b);
+  Serial.print("C = "); Serial.println(c);
+  Serial.print("Color Temp = "); Serial.println(colorTemp);
+  Serial.print("Lux = "); Serial.println(lux);
+
   uint16_t vis = tsl2591.getLuminosity(TSL2591_VISIBLE);
   uint16_t ir = tsl2591.getLuminosity(TSL2591_INFRARED);
 
+  Serial.println("TSL2591");
+
   float uv = veml6070.readUV();
+
+  Serial.println("VEML6070");
+  Serial.print("UV = "); Serial.println(uv);
+
+  delay(1000);
 
 }
